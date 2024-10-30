80688244603140e9e9ecd42654fd5ef2c13b7b3e
Hg-lab <siregy1222@gmail.com>
2024-10-30 23:18:18 +0900

    event_screens 스크롤뷰 적용

--- lib/screens/event_screens.dart
+++ lib/screens/event_screens.dart
@@ -19,67 +19,69 @@ class EventsPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Search bar
-            const CustomSearchBar(
-              hintText: "검색",
-            ),
-            const SizedBox(height: 16),
-            // Category buttons
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  _categoryButton('Art'),
-                  _categoryButton('Music'),
-                  _categoryButton('Food & Drink'),
-                  _categoryButton('Markets'),
-                ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Search bar
+              const CustomSearchBar(
+                hintText: "검색",
               ),
-            ),
-            const SizedBox(height: 16),
-            // Event section "For You"
-            const Text('For You',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 16),
-            const EventCard(
-              title: 'Exhibitions in San Francisco',
-              subtitle: 'You might like these',
-              imageUrl:
-                  'https://yeyak.seoul.go.kr/web/common/file/FileDown.do?file_id=1708587459850DATOW92X2UBMKCTKJU85WVIEL',
-            ),
-            const SizedBox(height: 16),
-            const EventCard(
-              title: 'Live music in SF',
-              subtitle: 'Today · 8 events',
-              imageUrl:
-                  'https://yeyak.seoul.go.kr/web/common/file/FileDown.do?file_id=17261001711188BN8YGBEHEGLSF0LPVC6AKPDE',
-            ),
-            const SizedBox(height: 16),
-            const EventCard(
-              title: 'Bay Area weekend markets',
-              subtitle: 'Tomorrow · 2 events',
-              imageUrl:
-                  'https://yeyak.seoul.go.kr/web/common/file/FileDown.do?file_id=1728693015139BXTFE6DZGE2YT6I4DSD589I9C',
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.location_on),
-              label: const Text('Nearby Events'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey[200],
-                foregroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+              const SizedBox(height: 16),
+              // Category buttons
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    _categoryButton('Art'),
+                    _categoryButton('Music'),
+                    _categoryButton('Food & Drink'),
+                    _categoryButton('Markets'),
+                  ],
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 16),
+              // Event section "For You"
+              const Text('For You',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 16),
+              const EventCard(
+                title: 'Exhibitions in San Francisco',
+                subtitle: 'You might like these',
+                imageUrl:
+                    'https://yeyak.seoul.go.kr/web/common/file/FileDown.do?file_id=1708587459850DATOW92X2UBMKCTKJU85WVIEL',
+              ),
+              const SizedBox(height: 16),
+              const EventCard(
+                title: 'Live music in SF',
+                subtitle: 'Today · 8 events',
+                imageUrl:
+                    'https://yeyak.seoul.go.kr/web/common/file/FileDown.do?file_id=17261001711188BN8YGBEHEGLSF0LPVC6AKPDE',
+              ),
+              const SizedBox(height: 16),
+              const EventCard(
+                title: 'Bay Area weekend markets',
+                subtitle: 'Tomorrow · 2 events',
+                imageUrl:
+                    'https://yeyak.seoul.go.kr/web/common/file/FileDown.do?file_id=1728693015139BXTFE6DZGE2YT6I4DSD589I9C',
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.location_on),
+                label: const Text('Nearby Events'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[200],
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
